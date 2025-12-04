0a3fb24a22169043639254ddaa82e4ae4cbc5677
OLEKSII\semen <lexasemenov467@gmail.com>
2025-12-04 17:35:16 +0200

    controllers-overview

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.set('query parser', 'extended'); // для роботи з складними queries масиви і так далі допомогає на парсити 
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
